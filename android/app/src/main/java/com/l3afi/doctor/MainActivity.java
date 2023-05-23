f97f548259ae694f96d77a55aee07f085ce77cd4
Moustapha LY <moustaphaly1996@gmail.com>
2023-05-23 01:28:29 +0000

    design finished

---
+++ android/app/src/main/java/com/l3afi/doctor/MainActivity.java
@@ -0,0 +1,6 @@
+package com.l3afi.doctor;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const wColor = Colors.white;
+const bColor = Colors.black;
+const pColor = Color(0xFF0C84FF);
+const sdColor = Colors.black12;
+
